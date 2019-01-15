f5d9ca91697d8488970ade04dd66e9795af9e834
Ferdinando Picarella <ferpic87@hotmail.it>
2019-01-15 23:39:53 +0100

    aggiunta pulsante vendi

--- Router.js
+++ Router.js
@@ -3,13 +3,14 @@ import { Scene, Router,Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import FindTicketForm from './components/FindTicketForm';
+import SellTicketForm from './components/SellTicketForm';
 import List from './components/List';
 import Create from './components/Create';
 import TicketEdit from './components/TicketEdit';
 
 const RouterComponent = () => {
   return(
-    <Router sceneStyle={{paddingTop: 65}}>
+    <Router sceneStyle={{paddingTop: 25}}>
       <Scene key="root" hideNavBar>
         <Scene key="auth">
           <Scene key="login" component={LoginForm} title="Autenticazione"/>
@@ -27,6 +28,7 @@ const RouterComponent = () => {
           />
           <Scene key = "TicketCreate" component={Create} title="Aggiungi biglietto" />
           <Scene key = "FindTicket" component={FindTicketForm} title="Ricerca biglietto" />
+          <Scene key = "SellTicket" component={SellTicketForm} title="Vendi biglietto" />
           <Scene key = "TicketEdit" component={TicketEdit} title="Modifica" />
         </Scene>
       </Scene>
